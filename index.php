3153b99ad70899d1c59cb9e523ee98e95c28948e
Maja Mitic <majamitic774@gmail.com>
2024-05-16 20:58:22 +0200

    make css and html

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
-<?php
-echo "bla";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <h1>Welcome to My Website</h1>
+
+    <div class="card">
+        <a href="mysites.php">
+            <img src="open.jpg" alt="My Sites">
+            <div class="container">
+                <h4><b>News</b></h4>
+                <p>Click here to visit my site</p>
+            </div>
+        </a>
+    </div>
+
+</body>
+
+</html>
