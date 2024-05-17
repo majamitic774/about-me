a316411e71ae57cb7cf9e3d2a3fc74723c4c164c
Maja Mitic <majamitic774@gmail.com>
2024-05-17 13:17:14 +0200

    add footer and change css and html

--- index.php
+++ index.php
@@ -10,15 +10,18 @@
 
 <body>
 
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
+    <header>
+        <div class="logo">ABOUT ME</div>
 
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
     <h1>Welcome to My Website</h1>
 
     <div class="card">
@@ -30,6 +33,29 @@
             </div>
         </a>
     </div>
+
+    <footer>
+        <div class="footer-container">
+            <div class="footer-section">
+                <h4>Contact Us</h4>
+                <p>Email: info@mywebsite.com</p>
+                <p>Phone: +123 456 7890</p>
+            </div>
+            <div class="footer-section">
+                <h4>Follow Us</h4>
+                <a href="#">Facebook</a>
+                <a href="#">Twitter</a>
+                <a href="#">Instagram</a>
+            </div>
+            <div class="footer-section">
+                <h4>About</h4>
+                <p>About the company and mission statement.</p>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>&copy; 2024 My Website. All rights reserved.</p>
+        </div>
+    </footer>
 
 </body>
 
